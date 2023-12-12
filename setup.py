9b00b7057c3ebd89310739fbdf1b25d237690988
Martin老師 <tpemartin@gmail.com>
2023-12-12 16:30:32 +0800

    update v1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gamepy',
-    version='1.0.2',
+    version='1.0.3',
     description='Description of your package',
     author='Your Name',
     packages=find_packages(),

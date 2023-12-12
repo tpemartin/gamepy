f1c1171a723aaa5752bd10a0a6a92c0167cb32bf
Martin老師 <tpemartin@gmail.com>
2023-12-12 16:36:11 +0800

    v1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gamepy',
-    version='1.0.3',
+    version='1.0.4',
     description='Description of your package',
     author='Your Name',
     packages=find_packages(),
